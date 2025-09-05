15e8d8854aa71bd7f98fc8ad33eedd73749f182c
thomasyxg <49958923+thomasyxg@users.noreply.github.com>
2025-09-05 14:49:01 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_RNygliZ84', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1c9ugsuppq0fhms7ev0ev417ft', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_nGfa6Fl3G', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7ss7lgtlco4hinqhkh20lkd6pt', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
